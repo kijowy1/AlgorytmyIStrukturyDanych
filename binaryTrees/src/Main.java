46993194ba69ff1eebc204ce9c65db54a0c5b510
kijowy1 <damian.aleksander.w@gmail.com>
2026-04-09 14:26:28 +0200

    node Commit

--- binaryTrees/src/Main.java
+++ binaryTrees/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+    }
+}
+class Node {
+    int key;
+    Node left,right;
+
+    public Node(int item){
+        key = item;
+        left = right = null;
     }
 }
